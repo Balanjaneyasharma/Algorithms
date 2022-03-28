3b26061877eff7473ab006aab0d29e3f83d96362
Balu Shrama <69688211+Balanjaneyasharma@users.noreply.github.com>
2022-03-28 18:10:29 +0530

    Update SegementedSieve.cpp
    When  R - L  Range Exceeds 10^6

--- SegementedSieve.cpp
+++ SegementedSieve.cpp
@@ -43,7 +43,7 @@ int main()
     for(auto it: dj)
     {
       int firstmul=(l/it)*it<l ? ((l/it)+1)*it:(l/it)*it;// finding first multiple of each prime number in given range
-      for(int j=max(firstmul,it*it);j<=r;j+=it)
+      for(int j=firstmul;j<=r;j+=it)
       {
         dummy[j-l]=0; //marking them non-prime
       }
